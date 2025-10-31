c8bb359eaf1ba7b21362632691cd01f0dd6f1f72
toia-19 <victoriaangelinadiaz@gmail.com>
2025-10-30 22:02:09 -0300

    add: Agregué ruta de componente "speaking" (revisar después cómo vamos a definir rutas)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SpeakingExerciseComponent } from './shared/speaking-exercise/speaking-exercise.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     loadChildren: () =>
       import('./features/login/login.module').then((m) => m.LoginPageModule),
   },
+  // Ejercicios
+  {
+    path: 'speaking-exercise', component: SpeakingExerciseComponent
+  }
 ];
 
 @NgModule({
